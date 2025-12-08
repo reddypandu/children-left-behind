23c3fd4b2d308a68d25b1e5cc6027df653ad776f
Patternscompany <pandureddypatterns@gmail.com>
2025-12-08 11:13:34 +0530

    pp

--- src/components/aboutHero/index.js
+++ src/components/aboutHero/index.js
@@ -4,17 +4,22 @@ import imgHero from "../../images/about/About.png"
 
 const AboutHero = () => {
     return (
-        <section className="wpo-about-area about-page-hero">
-            <div className="container  ourmissionn">
-                <div className="row align-items-center">
-                    <div className="col-md-6">
-                        <h2 className="wpo-section-title about">Together, let’s build a better future</h2>
-                        <p>Driven by our core values and visionary leadership, we are dedicated to creating a brighter future for children. As a committed non-profit organization, we strive to turn our ambitious vision into reality.</p>
-                    </div>
-                    <div className="col-md-6 img">
-                        <img src={imgHero} alt=""/>
-                    </div>
+        <section className="about-hero-section">
+            <div className="about-hero-container">
+                
+                <div className="about-text">
+                    <h2>Together, let’s build a better future</h2>
+                    <p>
+                        Driven by our core values and visionary leadership, we are dedicated 
+                        to creating a brighter future for children. As a committed non-profit 
+                        organization, we strive to turn our ambitious vision into reality.
+                    </p>
                 </div>
+
+                <div className="about-hero-img">
+                    <img src={imgHero} alt="about-hero" />
+                </div>
+
             </div>
         </section>
     )
